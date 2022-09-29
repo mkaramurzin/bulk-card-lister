ee922781334bd3b7581e255e6bcfcfaec5c0fdae
mkaramurzin <mkaramurzin02@gmail.com>
2022-09-29 12:15:49 -0700

    html edits

--- BulkLister/static/BulkLister/index.js
+++ BulkLister/static/BulkLister/index.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+qdocument.addEventListener('DOMContentLoaded', function() {
     document.querySelector("#graded").addEventListener('change', function() {
         let select = document.querySelector('#graded')
         let value = select.options[select.selectedIndex].value;
